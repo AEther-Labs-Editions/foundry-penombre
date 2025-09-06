9f5d887a1152327cd7b89456f5d05416a6f50ec9
Christophe Laudon <ch.laudon@gmail.com>
2025-09-06 17:33:31 +0200

    Affichage des messages d'erreurs dans la console, rollDialog fenêtre des atouts extensible

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const path = require('path');
 
 /* ----------------------------------------- */
 /*  Compile LESS
+/*  Affiche les erreurs dans la console
 /* ----------------------------------------- */
 function compileLESS() {
   return gulp.src("styles/penombre.less")
-      .pipe(less())
-      .pipe(gulp.dest("css"))
+    .pipe(less({
+      paths: [path.join(__dirname, 'styles')]
+    }))
+    .on('error', function(err) {
+      console.error('Erreur LESS:', err.message);
+      this.emit('end');
+    })
+    .pipe(gulp.dest("./css"));
 }
+
 const css = gulp.series(compileLESS);
 
 /* ----------------------------------------- */
 /*  Watch Updates
 /* ----------------------------------------- */
-const SIMPLE_LESS = ["styles/*.less"];
+const SIMPLE_LESS = ["styles/**/*.less"];
 
 function watchUpdates() {
   gulp.watch(SIMPLE_LESS, css);
@@ -25,7 +34,7 @@ function watchUpdates() {
 /* ----------------------------------------- */
 
 exports.default = gulp.series(
-    gulp.parallel(css),
-    watchUpdates
+  gulp.parallel(css),
+  watchUpdates
 );
 exports.css = css;
